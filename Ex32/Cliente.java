28be73bbc67d8f7ce10d3f2869bbc64d0209c277
Lucas Hoepers <100244062+lucashoepersf@users.noreply.github.com>
2022-05-05 22:54:18 -0300

    Add files via upload

---
+++ Ex32/Cliente.java
@@ -0,0 +1,9 @@
+package Ex32;
+
+public class Cliente {
+	
+	public int id;
+	public String nome;
+	public int idade;
+	public String email;
+}
---
+++ Ex32/Programa.java
@@ -0,0 +1,33 @@
+package Ex32;
+
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+
+		Cliente[] listaCliente = new Cliente[5];
+		for(int i=0;i<=4;i++){
+		Cliente a = new Cliente();
+		
+		System.out.printf("Digite o id do cliente: ");
+		a.id=ler.nextInt();
+		System.out.printf("Digite o nome do cliente: ");
+		a.nome=ler.next();
+		System.out.printf("Digite a idade do cliente: ");
+		a.idade=ler.nextInt();
+		System.out.printf("Digite o email do cliente: ");
+		a.email=ler.next();
+		listaCliente[i]=a;
+			if(i==4) {
+			for(int j=0;j<=4;j++) {
+		    if(listaCliente[j].idade>=18) {
+			System.out.printf("\nCliente %s, id %d, email %s, idade %d.\n",listaCliente[j].nome,listaCliente[j].id,listaCliente[j].email,listaCliente[j].idade);
+			}
+			}
+			}
+	
+		}
+	}
+}
